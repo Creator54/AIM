b65a32d29c3513a18a74f067053a2d0d1a084da8
Creator54 <hi.creator54@gmail.com>
2020-05-11 10:56:50 +0530

    beautify
    Signed-off-by: Creator54 <hi.creator54@gmail.com>

--- start.sh
+++ start.sh
@@ -1,78 +1,79 @@
 #!/bin/bash
 
-f1=tmp
-f2=system
-sys=$f1/system.img
-sparse=$f1/system_sparse.img
+dir=system
+sys=tmp/system.img
+sparse=tmp/system_sparse.img
 
 clear
 
-echo "#######################################"
-echo "         Android Image Modifier        "
-echo "#######################################"
+echo "#########################################"
+echo "										   "
+echo "        Android Image Modifier           "
+echo " 		       By Creator54			       "
+echo "										   "
+echo "#########################################"
 echo
 zip_file="$(ls -a | grep .zip)"
 if [[ -e "$zip_file" ]]; then
-	mkdir $f1 $f2
-	unzip $zip_file -d $f1
-	loc=$f1
-	if [[ -e "$loc/system.new.dat.br" ]]; then
-		brotli -f -d $loc/*.br
+	echo ROM found
+	mkdir tmp $dir
+	unzip $zip_file -d tmp | grep Archive
+	echo zip extracted.
+	if [[ -e "tmp/system.new.dat.br" ]]; then
+		brotli -f -d tmp/*.br
 	fi
-	file1=$loc/system.transfer.list
-	file2=$loc/system.new.dat
+	file1=tmp/system.transfer.list
+	file2=tmp/system.new.dat
 
 	if [[ -f "$file1" ]] && [[ -f "$file2" ]]; then
-	
+
 		echo
 		echo "Decompressing DAT (sparse data) -> EXT4 (raw image)"
-		echo 
-	
+		echo
+
 		./sdat2img/sdat2img.py $file1 $file2 $sys | grep Done!
-	
-		sudo mount -t ext4 -o loop $sys $f2
+
+		sudo mount -t ext4 -o loop $sys $dir
 		echo
-		echo "Image mounted at $f2"
-		echo
-		echo "Now make required changes to the image."
-		echo
-		
-		read -p "Press Enter to start repacking..." key
+		echo "Now u can start editing $dir directory."
+		echo 
+		read -p "Press enter to start repacking..." key
 		if [ "$key" = '' ]; then
-    		sudo umount $f2
+    		sudo umount $dir
     		echo " "
     		echo Image unmounted
     		img2simg $sys $sparse
     		echo
-    		echo Converting to sparse_img complete.
-   		 	echo
-	    	if [[ -e "$loc/system.new.dat.br" ]]; then
-    			./img2sdat/img2sdat.py $sparse -o $f1 -v 4
+    		echo Converting to sparse_img ...
+	    	if [[ -e "tmp/system.new.dat.br" ]]; then
+    			./img2sdat/img2sdat.py $sparse -o tmp -v 4 | grep Done
     			echo
-    			echo "System supports brotli compression ."
+    			echo "System supports brotli compression."
     			echo
     			echo " 1 - fastest/least compression"
     			echo " 6 - default aosp compression"
     			echo "11 - slowest/max compression"
     			echo
     			read -p "Enter compression level(1-11): " l
-				brotli -f -$l $f1/system.new.dat
-				echo
+				brotli -f -$l tmp/system.new.dat
+				echo Brotli compression complete.
 			else
-				./img2sdat/img2sdat.py $sparse -o $f1
+				./img2sdat/img2sdat.py $sparse -o tmp | grep
 			fi
     	fi
 	fi
-	echo Zipping up
 	echo
-	if [[ -e "$loc/system.new.dat.br" ]]; then
-		rm $f1/system.new.dat
+	echo Finalizing New_$zip_file ...
+	echo
+	if [[ -e "tmp/system.new.dat.br" ]]; then
+		rm tmp/system.new.dat
 	fi
-	rm $sys $sparse && cd $f1 && zip test_ROM.zip -r *
-	mv test_ROM.zip ../ && cd ../ && rm -rf $f1 $f2
+	rm $sys $sparse && cd tmp 
+	zip New_$zip_file -r *
+	mv New_$zip_file ../ && cd ../ && rm -rf tmp $dir
 	echo
 	echo "All Done. Time for testing :)"
 	echo
 else
-	echo "Copy ROM zip to this folder."
+	echo "Copy ROM zip to this dir."
 fi
